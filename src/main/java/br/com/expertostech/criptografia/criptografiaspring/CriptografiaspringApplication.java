2e77daad62ddc0642f400b4e371c014e440e8955
Samuel M. dos Santos <samuelmacedo2011@gmail.com>
2021-12-10 20:34:46 -0300

    Criação de Validação de Senha Criptografada

--- src/main/java/br/com/expertostech/criptografia/criptografiaspring/CriptografiaspringApplication.java
+++ src/main/java/br/com/expertostech/criptografia/criptografiaspring/CriptografiaspringApplication.java
@@ -3,6 +3,9 @@ package br.com.expertostech.criptografia.criptografiaspring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class CriptografiaspringApplication {
@@ -11,4 +14,9 @@ public class CriptografiaspringApplication {
 		SpringApplication.run(CriptografiaspringApplication.class, args);
 	}
 
+	@Bean
+	public PasswordEncoder getPasswordEncoder(){
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		return encoder;
+	}
 }
