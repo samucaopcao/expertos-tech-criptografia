7a3a1979d382aef0390f7941dc49d00b9a8196fa
Samuel M. dos Santos <samuelmacedo2011@gmail.com>
2021-12-10 19:17:44 -0300

    Criação do Projeto

---
+++ src/main/java/br/com/expertostech/criptografia/criptografiaspring/CriptografiaspringApplication.java
@@ -0,0 +1,13 @@
+package br.com.expertostech.criptografia.criptografiaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriptografiaspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CriptografiaspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/expertostech/criptografia/criptografiaspring/CriptografiaspringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.expertostech.criptografia.criptografiaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriptografiaspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
