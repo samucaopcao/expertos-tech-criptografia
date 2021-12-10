d69f43ef09e3e8681928ac61dad77860cb37733e
Samuel M. dos Santos <samuelmacedo2011@gmail.com>
2021-12-10 20:04:54 -0300

    Criação de Entidade Usuario e configuração do Spring Security

--- src/main/java/br/com/expertostech/criptografia/criptografiaspring/CriptografiaspringApplication.java
+++ src/main/java/br/com/expertostech/criptografia/criptografiaspring/CriptografiaspringApplication.java
@@ -2,8 +2,9 @@ package br.com.expertostech.criptografia.criptografiaspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class CriptografiaspringApplication {
 
 	public static void main(String[] args) {
